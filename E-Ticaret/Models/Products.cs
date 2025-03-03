8fc553930ff6f80ef5e76a4e6c49fa13427d0c35
ecevik1 <erenaycevik@outlook.com.tr>
2025-03-03 23:55:40 +0300

    feat: Products modelini nullable referans türlerini ve doğrulamayı ele alacak şekilde güncelle
    - ProductName, ProductDescription, ProductPicture ve Category nullable yapıldı.
    - ProductName, ProductDescription ve ProductPicture string.Empty ile başlatıldı.
    - Doğrulama için [Required] veri ek açıklamaları eklendi.

--- E-Ticaret/Models/Products.cs
+++ E-Ticaret/Models/Products.cs
@@ -5,20 +5,30 @@ namespace E_Ticaret.Models
 {
     public class Products
     {
-
         [Key]
-        public int ProductId{ get; set; }
-        public string? ProductName { get; set; }=string.Empty;
-        public string? ProductDescription { get; set; }= string.Empty;
-        public int? ProductPrice { get; set; } = 0;
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required")]
+        public string? ProductName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Product Description is required")]
+        public string? ProductDescription { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Product Price is required")]
+        public int ProductPrice { get; set; } = 0;
+
         public string? ProductPicture { get; set; } = string.Empty;
-        public int? CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Category ID is required")]
+        public int CategoryId { get; set; }
+
         public int? ProductCode { get; set; }
-        public virtual Categories Category { get; set; } //kategori tablosu ile ilişkilendirme yapılır.
+
+        public virtual Categories? Category { get; set; } // kategori tablosu ile ilişkilendirme yapılır.
 
         [NotMapped]
         public IFormFile? ImageUpload { get; set; } // dosya yükleme işlemi için kullanılır.
-
-
     }
 }
+
+
