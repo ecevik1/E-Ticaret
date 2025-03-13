cbd18c1c39210e5f595a63b572f248007ebc10c6
ecevik1 <erenaycevik@outlook.com.tr>
2025-03-13 14:41:03 +0300

    Layout güncellendi ve SliderList bileşeni eklendi

---
+++ E-Ticaret/Component/SliderList/SliderList.cs
@@ -0,0 +1,20 @@
+﻿using E_Ticaret.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Ticaret.Component.SliderList
+{
+    public class SliderList:ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SliderList(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public IViewComponentResult Invoke()//SliderList.cshtml sayfasına veri gönderme işlemi
+        {
+            var result = _context.Slider.ToList();
+            return View(result);
+        }
+    }
+}
