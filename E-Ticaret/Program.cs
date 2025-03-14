06d93532e2a24d00064eefaef4f5ae1c4daf70e1
ecevik1 <erenaycevik@outlook.com.tr>
2025-03-15 01:59:25 +0300

    Session İşlemlerini kullanabilmek için Yapılandırıldı.

--- E-Ticaret/Program.cs
+++ E-Ticaret/Program.cs
@@ -9,26 +9,28 @@ namespace E_Ticaret
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddDbContext<ApplicationDbContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));});
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
-            // Add services to the container.
+            // Hizmetleri konteynere ekleyin.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddSession(); // Oturum hizmetlerini ekleyin
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // HTTP istek hatt?n? yap?land?r?n.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseSession(); // Oturum yönetimini etkinle?tirin
             app.UseAuthorization();
 
             app.MapControllerRoute(
